60e7452f0dc4c15fa029183af0b9f31117a8ff2b
Artur Tryc <tryc.artur@gmail.com>
2024-06-07 00:16:19 +0200

    ProjectController.

---
+++ iTask/Controllers/ProjectController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using iTask.Models; 
+
+public class ProjectController : Controller
+{
+    private readonly ILogger<ProjectController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ProjectController(ILogger<ProjectController> logger, IUnitOfWork unitOfWork)
+    {
+        _logger = logger;
+        _unitOfWork = unitOfWork;
+    }
+
+    public ActionResult Index(){
+        return View();
+    }
+
+}
