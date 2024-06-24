e8e7991104666879ddf7836a526948fb1d8a6fe2
Artur Tryc <tryc.artur@gmail.com>
2024-06-24 23:56:47 +0200

    DbInitializer

--- iTask/Data/Initializer/DbInitializer.cs
+++ iTask/Data/Initializer/DbInitializer.cs
@@ -1,4 +1,24 @@
+using Microsoft.AspNetCore.Identity;
+using iTask.Data;
+
 public class DbInitializer : IDbInitializer
 {
+
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly ApplicationDbContext _db; 
+    public DbInitializer(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext db)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _db = db;
+    } 
+    public void Initialize()
+    {
+
+
+
+    }
+
 
 }
--- iTask/Data/Initializer/IDbInitializer.cs
+++ iTask/Data/Initializer/IDbInitializer.cs
@@ -1,4 +1,6 @@
 public class IDbInitializer
 {
 
+    public void Initialize();
+
 }
