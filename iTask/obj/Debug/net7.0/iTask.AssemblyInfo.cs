959f250fa9a2dee028b22bdcb019ee2559c52706
Artur Tryc <tryc.artur@gmail.com>
2024-10-21 08:22:32 +0200

    Droplist to fix.

--- iTask/obj/Debug/net7.0/iTask.AssemblyInfo.cs
+++ iTask/obj/Debug/net7.0/iTask.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d3acb856cdeed1f85fb70bf3000be1d145afca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590d751957e5c9330af9ac7e4c5a51a798d1eca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("iTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
