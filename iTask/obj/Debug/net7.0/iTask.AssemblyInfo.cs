e73a422e8f5616ca7ba1ad86603c3db3f640d2d9
Artur Tryc <tryc.artur@gmail.com>
2024-10-21 23:12:46 +0200

    Select AspFor try.

--- iTask/obj/Debug/net7.0/iTask.AssemblyInfo.cs
+++ iTask/obj/Debug/net7.0/iTask.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590d751957e5c9330af9ac7e4c5a51a798d1eca6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+959f250fa9a2dee028b22bdcb019ee2559c52706")]
 [assembly: System.Reflection.AssemblyProductAttribute("iTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
