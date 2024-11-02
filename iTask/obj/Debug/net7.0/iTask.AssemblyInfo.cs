9f8a53bf3c439e15dc3707c250913f1cbef8883e
Artur Tryc <tryc.artur@gmail.com>
2024-11-02 21:56:38 +0100

    View Assignment Edit Changes.

--- iTask/obj/Debug/net7.0/iTask.AssemblyInfo.cs
+++ iTask/obj/Debug/net7.0/iTask.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec8c474c22f44434b9c1102064d4d4bbfeaf4159")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa557fbe7639cf4bd810937392121af3a0b7fd56")]
 [assembly: System.Reflection.AssemblyProductAttribute("iTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
