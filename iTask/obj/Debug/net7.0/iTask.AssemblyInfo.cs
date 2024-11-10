944f9aac511c0e8dcc1d7f52c3597b130a469c64
Artur Tryc <tryc.artur@gmail.com>
2024-11-11 00:02:19 +0100

    Final version.

--- iTask/obj/Debug/net7.0/iTask.AssemblyInfo.cs
+++ iTask/obj/Debug/net7.0/iTask.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6acdb9c9744b4e6340048730e7fd41605e8794f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b93f558de63176207705f192d5fefc4c4ed751b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("iTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
